a59f2748f33ef01cad9a3d7809f5c5ba045ae9dd
tirasdor <tirasdor@gmail.com>
2022-01-06 15:13:01 +0200

    first commit

---
+++ mac_changer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+import re
+
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    options, args = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a MAC address, use --help for more info")
+    return options
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC addess for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    
+    if mac_result:
+        return mac_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
+options = get_args()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+change_mac(options.interface, options.new_mac)
+new_mac = get_current_mac(options.interface)
+if new_mac == options.new_mac:
+    print("[+] MAC address was sucessfully changed!")
+    print("[=] New MAC address: " + new_mac)
+else:
+    print("[-] Caution! Process faild! MAC address did not get changed.")
